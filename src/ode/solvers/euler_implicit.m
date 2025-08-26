00d94171348ab665d4d3c78bcd47c913020c4691
ArgonautAstra <andreaspin0147@gmail.com>
2025-08-26 11:05:27 +0200

    update ode

--- src/ode/solvers/euler_implicit.m
+++ src/ode/solvers/euler_implicit.m
@@ -42,20 +42,3 @@ function Y = euler_implicit(f, t, y0, tol, maxit)
         Y(:,n+1) = y;
     end
 end
-    
-%% ---- Utils ----
-    
-function J = jacobian(F, y)
-    % Finite-difference Jacobian of F at y
-    D = numel(y);
-    J = zeros(D,D);
-    eps0 = 1e-6 * max(1, norm(y,2));
-
-    for i = 1:D
-        e = zeros(D,1); e(i) = 1;
-        h = eps0 * (1 + abs(y(i)));
-        yph = y + h*e;
-        ymh = y - h*e;
-        J(:,i) = (F(yph) - F(ymh)) / (2*h);
-    end
-end
